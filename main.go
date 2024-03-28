99b2cd81e790ab44157fa256e64098aaafbbc6ec
MarihkaDevochka <150847067+MarihkaDevochka@users.noreply.github.com>
2024-03-28 10:33:34 +0200

    Update main.go

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 
 	router := http.NewServeMux()
 	c := cors.New(cors.Options{
-		AllowedOrigins: []string{"http://localhost:4000", "http://localhost:3000"},
+             AllowedOrigins: []string{"http://localhost:4000", "http://localhost:3000", "https://golang-on-koyeb-mankago.koyeb.app/"},
 	})
 
 	db, err := db.DBConnection()

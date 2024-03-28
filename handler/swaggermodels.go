566f7e8bf1e8fe188d4d5f7e08849b9d17697052
MarihkaDevochka <150847067+MarihkaDevochka@users.noreply.github.com>
2024-03-28 11:57:11 +0200

    Update swaggermodels.go

--- handler/swaggermodels.go
+++ handler/swaggermodels.go
@@ -24,7 +24,7 @@ type ChapterSwag struct {
 	Img       []string  `json:"genres" db:"img"`
 	Name      string    `json:"name"`
 	AnimeName string    `json:"animeName" db:"animeName"`
-	CreatedAt time.Time `json:"created" db:"createdAt"`
+	CreatedAt time.Time `json:"createdAt" db:"createdAt"`
 }
 
 type UserSwag struct {
